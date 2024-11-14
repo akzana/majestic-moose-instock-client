d7c9d4c8ba657dd76ebf49d6810f56229584b343
Aswini Viswanath <aswiniviswanath97@gmail.com>
2024-11-14 16:58:26 -0500

    table styled for mobile

--- pages/WarehouseList/WarehouseList.jsx
+++ pages/WarehouseList/WarehouseList.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './WarehouseList.scss';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./WarehouseList.scss";
+import searchIcon from '../../src/assets/Icons/search-24px.svg';
 
-const API_URL = 'http://localhost:8080/api/warehouses';
+const API_URL = "http://localhost:8080/api/warehouses";
 
 const WarehouseList = () => {
   const [warehouses, setWarehouses] = useState([]);
@@ -14,51 +15,87 @@ const WarehouseList = () => {
         const data = await response.json();
         setWarehouses(data);
       } catch (error) {
-        console.error('Error fetching warehouses:', error);
+        console.error("Error fetching warehouses:", error);
       }
     };
     fetchWarehouses();
   }, []);
 
   return (
-    <div className="warehouse__list">
-      <h2 className="warehouse__heading">Warehouses</h2>
-      
+    <div className="warehouse">
+     <div className="warehouse">
+      <h2 className="warehouse__title">Warehouses</h2>
       <div className="warehouse__search">
         <input
           type="text"
-          className="warehouse__search-input"
-          placeholder="Search Warehouses"
+          className="warehouse__input"
+          placeholder="Search..."
           disabled
         />
+        <img src={searchIcon} alt="Search Icon" className="warehouse__icon" />
       </div>
+      
+      <Link to="/warehouses/add" className="warehouse__add-button primary-button">
+        + Add New Warehouse
+      </Link>
+    </div>
 
-      <table className="warehouse__table">
-        <thead className="warehouse__table-header">
-          <tr className="warehouse__table-row">
-            <th className="warehouse__table-heading">Warehouse</th>
-            <th className="warehouse__table-heading">Address</th>
-            <th className="warehouse__table-heading">Contact Name</th>
-            <th className="warehouse__table-heading">Contact Information</th>
-            <th className="warehouse__table-heading">Actions</th>
+      <table className="warehouse__list">
+        <thead className="warehouse__list-header">
+          <tr className="warehouse__list-row">
+            <th className="warehouse__list-heading">Warehouse</th>
+            <th className="warehouse__list-heading">Address</th>
+            <th className="warehouse__list-heading">Contact Name</th>
+            <th className="warehouse__list-heading">Contact Information</th>
+            <th className="warehouse__list-heading">Actions</th>
           </tr>
         </thead>
         <tbody className="warehouse__body">
           {warehouses.map((warehouse) => (
-            <tr key={warehouse.id} className="warehouse-list__body-row">
-              <td className="warehouse__body-cell">{warehouse.warehouse_name}</td>
-              <td className="warehouse__body-cell">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</td>
-              <td className="warehouse__body-cell">{warehouse.contact_name}</td>
+            <tr key={warehouse.id} className="warehouse__body-row">
+              <div className="warehouse__body-cols">
+              <div className="warehouse__body-col1">
               <td className="warehouse__body-cell">
+                <span className="warehouse__cell-header">Warehouse</span>
+                <Link  to={`/warehouses/${warehouse.id}`}
+                  className="warehouse__link">
+                  {warehouse.warehouse_name} 
+                </Link>
+              </td>
+              <td className="warehouse__body-cell">
+                <span className="warehouse__cell-header">Address</span>
+                {`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}
+              </td>
+              </div>
+              <div className="warehouse__body-col2">
+              <td className="warehouse__body-cell">
+                <span className="warehouse__cell-header">Contact Name</span>
+                {warehouse.contact_name}
+              </td>
+              <td className="warehouse__body-cell">
+                <span className="warehouse__cell-header">
+                  Contact Information
+                </span>
                 <p className="warehouse__contact">
-                  Phone: {warehouse.contact_phone}
+                 {warehouse.contact_phone}
                 </p>
                 <p className="warehouse__contact-info">
-                  Email: <a href={`mailto:${warehouse.contact_email}`} className="warehouse__contact-email">{warehouse.contact_email}</a>
+                {" "}
+                  <a
+                    href={`mailto:${warehouse.contact_email}`}
+                    className="warehouse__contact-email" >
+                    {warehouse.contact_email}
+                  </a>
                 </p>
               </td>
+              </div>
+              </div>
               <td className="warehouse__body-cell warehouse__actions">
-                <Link to={`/warehouse/${warehouse.id}`} className="warehouse__edit">Edit</Link>
+                
+                <Link to={`/warehouse/${warehouse.id}`}
+                  className="warehouse__edit" >
+                  Edit
+                </Link>
                 <button className="warehouse__delete">Delete</button>
               </td>
             </tr>

d5f615dfc5be8c74a7f648c1eddee9af8183228e
Aswini Viswanath <aswiniviswanath97@gmail.com>
2024-11-15 16:29:58 -0500

    delete modal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./DeleteModal.scss";
+import closeIcon from "../../assets/Icons/close-24px.svg";
 
 const DeleteModal = ({ show, onClose, onConfirm }) => {
   if (!show) return null;
 
   return (
     <div className="modal-overlay">
+     
       <div className="modal">
-        <h2>Confirm Delete</h2>
+        <div className="modal__content">
+      <div className="modal-button modal-button--close" onClick={onClose}> 
+        <img src={closeIcon} alt="Close Icon" className="close__icon" />
+      </div>
+        <h2>Delete Washington warehouse?</h2>
         <p>Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to undo this action.</p>
+        </div>
         <div className="modal-actions">
           <button className="modal-button modal-button--cancel" onClick={onClose}>
             Cancel

c7951aefeb47fa883e7ae008a3e02d109789ef88
Juliet Chuon | itsjuliet <j-chuon@hotmail.com>
2024-11-15 14:24:30 -0800

    fix pages and paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
-import WarehouseList from "../pages/WarehouseList/WarehouseList";
-import WarehouseDetails from "../pages/WarehouseDetails/WarehouseDetails";
-import AddWarehouse from "../pages/AddWarehouse/AddWarehouse";
-import EditWarehouse from "../pages/EditWarehouse/EditWarehouse";
-import InventoryList from "../pages/InventoryPage/InventoryPage";
-import InventoryItem from "../pages/InventoryItemDetails/InventoryItemDetails";
-import AddInventoryItem from "../pages/AddInventoryItem/AddInventoryItem";
-import EditInventoryItem from "../pages/EditInventoryItem/EditInventoryItem";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import WarehouseList from "./pages/WarehouseList/WarehouseList";
+import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails";
+import AddWarehouse from "./pages/AddWarehouse/AddWarehouse";
+import EditWarehouse from "./pages/EditWarehouse/EditWarehouse";
+import InventoryList from "./pages/InventoryPage/InventoryPage";
+import InventoryItem from "./pages/InventoryItemDetails/InventoryItemDetails";
+import AddInventoryItem from "./pages/AddInventoryItem/AddInventoryItem";
+import EditInventoryItem from "./pages/EditInventoryItem/EditInventoryItem";
 import Footer from './components/footer/footer';
 
 function App() {
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./InventoryPage.scss"
-import InventoryHeader from '../../src/components/InventoryHeader/InventoryHeader';
-import InventoryList from '../../src/components/InventoryList/InventoryList';
+import InventoryHeader from '../../components/InventoryHeader/InventoryHeader';
+import InventoryList from '../../components/InventoryList/InventoryList';
 
 export default function InventoryPage() {
   return (

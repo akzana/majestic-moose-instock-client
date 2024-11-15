8f4e1f69bbf98d0d06d850f6fd74260daa86aa38
akzana <akzanaklepsch@outlook.com>
2024-11-14 19:41:22 -0800

    💄 add styling for InventoryHeader and InventoryPage; add ad hoc mixins

--- pages/InventoryPage/InventoryPage.jsx
+++ pages/InventoryPage/InventoryPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import InventoryList from '../../src/components/InventoryList/InventoryList'
+import React from 'react';
+import "./InventoryPage.scss"
+import InventoryHeader from '../../src/components/InventoryHeader/InventoryHeader';
+import InventoryList from '../../src/components/InventoryList/InventoryList';
 
 export default function InventoryPage() {
   return (
-    <div>
+    <div className='component'>
+      <InventoryHeader />
       <InventoryList />
     </div>
   )
--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -4,19 +4,18 @@ import DeleteButton from "../../assets/Icons/delete_outline-24px.svg";
 import EditButton from "../../assets/Icons/edit-24px.svg"
 import axios from 'axios';
 import { useState } from 'react';
+import "./InventoryList.scss"
 
 const baseURL = import.meta.env.VITE_URL;
 
 export default function InventoryList() {
     // conditional for warehouse.id
-    //axios to server
     const [inventory, setInventory] = useState([]);
 
     const getInventory = async () => {
         try {
             const response = await axios.get(`${baseURL}/api/inventories`);
             setInventory(response.data);
-            console.log(response.data);
 
         } catch (err) {
             console.error("Error retrieving inventory items", err);
@@ -32,20 +31,50 @@ export default function InventoryList() {
             <ul className="inventoryList">
                 {inventory.map((item) =>
                     <li className="inventoryList__item" key={item.id}>
-                        <p className="inventoryList__label">INVENTORY ITEM</p>
-                        <p className="inventoryList__item-title">{item.item_name}</p>
-                        <p className="inventoryList__category-label">CATEGORY</p>
-                        <p className="inventoryList__category">{item.category}</p>
-                        <p className="inventoryList__status-label">STATUS</p>
-                        <p className="inventoryList__status">{item.status}</p>
-                        <p className="inventoryList__qty-label">QTY</p>
-                        <p className="inventoryList__qty">{item.quantity}</p>
-                        <p className="inventoryList__warehouse-label">WAREHOUSE</p>
-                        <p className="inventoryList__warehouse">{item.warehouse_name}</p>
+                        <label
+                            for="item-name"
+                            className="inventoryList__item-label">INVENTORY ITEM
+                            <p
+                                className="inventoryList__item-name"
+                                name="item-name">{item.item_name}
+                            </p>
+                        </label>
+                        <label
+                            for="category"
+                            className="inventoryList__category-label">CATEGORY
+                            <p
+                                className="inventoryList__category"
+                                name="category">{item.category}
+                            </p>
+                        </label>
+                        <label
+                            for="status"
+                            className="inventoryList__status-label">STATUS
+                            <p
+                                className="inventoryList__status"
+                                name="status">{item.status}
+                            </p>
+                        </label>
+                        <label
+                            for="qty"
+                            className="inventoryList__qty-label">QTY
+                            <p
+                                className="inventoryList__qty"
+                                name="qty">{item.quantity}
+                            </p>
+                        </label>
+                        <label
+                            for="warehouse"
+                            className="inventoryList__warehouse-label">WAREHOUSE
+                            <p
+                                className="inventoryList__warehouse"
+                                name="warehouse">{item.warehouse_name}
+                            </p>
+                        </label>
                         <Link to="/"><img src={DeleteButton} alt="delete-button" /></Link>
-                        <Link><img src={EditButton} alt="" /></Link>
+                        <Link to="/inventory/edit/"><img src={EditButton} alt="edit inventory item button" /></Link>
                     </li>
-                )};
+                )}
             </ul>
         </div>
     )

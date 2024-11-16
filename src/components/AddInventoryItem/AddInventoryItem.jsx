15fc4351704354c52f89475fa0310fcb4a51cd42
teagenkew <96027598+teagenkew@users.noreply.github.com>
2024-11-15 22:53:54 -0700

    adds basic form function - no error state

--- src/components/AddInventoryItem/AddInventoryItem.jsx
+++ src/components/AddInventoryItem/AddInventoryItem.jsx
@@ -35,9 +35,35 @@ function AddInventoryItem() {
   const categories = inventoryItems.map((item) => item.category);
   const uniqueCategories = new Set(categories);
 
+  const [formData, setFormData] = useState({
+    warehouse_id: "",
+    item_name: "",
+    description: "",
+    category: "",
+    status: "",
+    quantity: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]:
+        name === "quantity" || name === "warehouse_id"
+          ? parseInt(value)
+          : value,
+    }));
+  };
+
   const handleSubmit = (event) => {
-    
-  }
+    event.preventDefault();
+    const finalData = {
+      ...formData,
+      warehouse_id: parseInt(formData.warehouse_id),
+      quantity: isNaN(formData.quantity) ? 0 : formData.quantity,
+    };
+    console.log(finalData);
+  };
   return (
     <section>
       <div className="add-new-item__header">
@@ -50,23 +76,39 @@ function AddInventoryItem() {
         </Link>
         <h1 className="add-new-item__title">Add New Inventory Item</h1>
       </div>
-      <form onSubmit="handleSubmit">
+      <form onSubmit={handleSubmit}>
         <div className="item-details">
           <h2 className="item-details__title">Item Details</h2>
           <label htmlFor="itemName">Item Name </label>
-          <input type="text" placeholder="Item Name" id="itemName" />
+          <input
+            type="text"
+            placeholder="Item Name"
+            id="itemName"
+            name="item_name"
+            value={formData.item_name}
+            onChange={handleChange}
+          />
 
           <label htmlFor="itemDescription">Item Description </label>
           <textarea
-            name="itemDescription"
+            name="description"
             id="itemDescription"
             placeholder="Please enter a brief item description..."
+            value={formData.description}
+            onChange={handleChange}
           ></textarea>
 
           <label htmlFor="category">Category </label>
-          <select name="category" id="category">
+          <select
+            name="category"
+            id="category"
+            value={formData.category}
+            onChange={handleChange}
+          >
             {[...uniqueCategories].map((category) => (
-              <option key={category}>{category}</option>
+              <option key={category} value={category}>
+                {category}
+              </option>
             ))}
           </select>
         </div>
@@ -74,17 +116,49 @@ function AddInventoryItem() {
           <h2 className="item-availability__title">Item Availability</h2>
           <label>Status </label>
           <label>
-            <input type="radio" name="status" id="status" />
-            In Stock{" "}
-          </label>{" "}
+            <input
+              type="radio"
+              value="In Stock"
+              name="status"
+              checked={formData.status === "In Stock"}
+              onChange={handleChange}
+            />
+            In Stock
+          </label>
           <label>
-            <input type="radio" name="status" id="status" />
+            <input
+              type="radio"
+              value="Out of Stock"
+              name="status"
+              checked={formData.status === "Out of Stock"}
+              onChange={handleChange}
+            />
             Out of Stock
           </label>
-          <label htmlFor="quantity">Quantity</label>
-          <input type="number" name="quantity" id="quantity" />
+          <div
+            className={
+              formData.status === "Out of Stock"
+                ? "item-availability__quantity-hidden"
+                : "item-availability__quantity-visible"
+            }
+          >
+            <label htmlFor="quantity">Quantity</label>
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              min="1"
+              value={formData.quantity}
+              onChange={handleChange}
+            />
+          </div>
           <label htmlFor="warehouse">Warehouse </label>
-          <select name="warehouse" id="warehouse">
+          <select
+            name="warehouse_id"
+            id="warehouse"
+            value={formData.warehouse_id}
+            onChange={handleChange}
+          >
             {warehouses.map((warehouse) => (
               <option key={warehouse.id} value={warehouse.id}>
                 {warehouse.warehouse_name}
@@ -93,12 +167,12 @@ function AddInventoryItem() {
           </select>
         </div>
         <div className="buttons">
-          <Link>
+          <Link to="/inventory">
             <button className="cancel-button" type="button">
               Cancel
             </button>
           </Link>
-          <button className="add-item-button" type="button">
+          <button className="add-item-button" type="submit">
             + Add Item
           </button>
         </div>

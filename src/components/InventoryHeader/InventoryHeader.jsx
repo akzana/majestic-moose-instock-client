3b96edd0b987e1d543b53088170b8aa436ac0e95
akzana <akzanaklepsch@outlook.com>
2024-11-17 14:16:38 -0800

    pair programming bug fixes

--- src/components/InventoryHeader/InventoryHeader.jsx
+++ src/components/InventoryHeader/InventoryHeader.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./InventoryHeader.scss";
 
+
 export default function InventoryHeader() {
+    const navigate = useNavigate();
     return (
         <div className='inventory'>
             <h1 className="inventory__heading">Inventory</h1>
             <input type="text" className="inventory__search-bar" placeholder='Search...'/>
-            <button className="inventory__button">+ Add New Item</button>
+            <button className="inventory__button" onClick={() => navigate("/inventory/add")}>+ Add New Item</button>
         </div>
     )
 }

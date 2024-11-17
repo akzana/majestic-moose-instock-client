cefb8e9c615045b65ae412500e31bcb7d263f1f5
akzana <akzanaklepsch@outlook.com>
2024-11-16 17:33:20 -0800

    update inventoryList styling

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -4,7 +4,7 @@ import DeleteButton from "../../assets/Icons/delete_outline-24px.svg";
 import EditButton from "../../assets/Icons/edit-24px.svg"
 import axios from 'axios';
 import { useState } from 'react';
-import "./InventoryList.scss"
+import "./InventoryList.scss";
 
 const baseURL = import.meta.env.VITE_URL;
 
@@ -38,48 +38,56 @@ export default function InventoryList() {
             <ul className="inventoryList">
                 {inventory.map((item) =>
                     <li className="inventoryList__item" key={item.id}>
-                        <label
-                            for="item-name"
-                            className="inventoryList__item-label">INVENTORY ITEM
-                            <p
-                                className="inventoryList__item-name"
-                                name="item-name">{item.item_name}
-                            </p>
-                        </label>
-                        <label
-                            for="category"
-                            className="inventoryList__category-label">CATEGORY
-                            <p
-                                className="inventoryList__category"
-                                name="category">{item.category}
-                            </p>
-                        </label>
-                        <label
-                            for="status"
-                            className="inventoryList__status-label">STATUS
-                            <p
-                                className="inventoryList__status"
-                                name="status">{item.status}
-                            </p>
-                        </label>
-                        <label
-                            for="qty"
-                            className="inventoryList__qty-label">QTY
-                            <p
-                                className="inventoryList__qty"
-                                name="qty">{item.quantity}
-                            </p>
-                        </label>
-                        <label
-                            for="warehouse"
-                            className="inventoryList__warehouse-label">WAREHOUSE
-                            <p
-                                className="inventoryList__warehouse"
-                                name="warehouse">{item.warehouse_name}
-                            </p>
-                        </label>
-                        <Link to="/"><img src={DeleteButton} alt="delete-button" /></Link>
-                        <Link to="/inventory/edit/"><img src={EditButton} alt="edit inventory item button" /></Link>
+                        <div className="inventoryList__info-column">
+                            <div className="inventoryList__column1">
+                                <label
+                                    for="item-name"
+                                    className="inventoryList__item-label">INVENTORY ITEM
+                                    <p
+                                        className="inventoryList__item-name"
+                                        name="item-name">{item.item_name}
+                                    </p>
+                                </label>
+                                <label
+                                    for="category"
+                                    className="inventoryList__category-label">CATEGORY
+                                    <p
+                                        className="inventoryList__category"
+                                        name="category">{item.category}
+                                    </p>
+                                </label>
+                            </div>
+                            <div className="inventoryList__column2">
+                                <label
+                                    for="status"
+                                    className="inventoryList__status-label">STATUS
+                                    <p
+                                        className="inventoryList__status"
+                                        name="status">{item.status}
+                                    </p>
+                                </label>
+                                <label
+                                    for="qty"
+                                    className="inventoryList__qty-label">QTY
+                                    <p
+                                        className="inventoryList__qty"
+                                        name="qty">{item.quantity}
+                                    </p>
+                                </label>
+                                <label
+                                    for="warehouse"
+                                    className="inventoryList__warehouse-label">WAREHOUSE
+                                    <p
+                                        className="inventoryList__warehouse"
+                                        name="warehouse">{item.warehouse_name}
+                                    </p>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="inventoryList__interaction">
+                            <Link to="/"><img src={DeleteButton} alt="delete-button" /></Link>
+                            <Link to="/inventory/edit/"><img src={EditButton} alt="edit inventory item button" /></Link>
+                        </div>
                     </li>
                 )}
             </ul>

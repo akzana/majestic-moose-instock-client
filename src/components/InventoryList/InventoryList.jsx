244cd9d91bf630fd546c3c47b4c1df3c3b7ed1e5
akzana <akzanaklepsch@outlook.com>
2024-11-16 19:36:14 -0800

    update styling for inventory component

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import DeleteButton from "../../assets/Icons/delete_outline-24px.svg";
-import EditButton from "../../assets/Icons/edit-24px.svg"
 import axios from 'axios';
 import { useState } from 'react';
+import sortIcon from "../../assets/Icons/sort-24px.svg";
+import rightAarrowIcon from "../../assets/Icons/chevron_right-24px.svg";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
 import "./InventoryList.scss";
 
 const baseURL = import.meta.env.VITE_URL;
 
 export default function InventoryList() {
-    // conditional for warehouse.id
+
     const [inventory, setInventory] = useState([]);
     const { id } = useParams();
-
 
     const getInventory = async () => {
         try {
@@ -35,62 +36,95 @@ export default function InventoryList() {
 
     return (
         <div>
-            <ul className="inventoryList">
-                {inventory.map((item) =>
-                    <li className="inventoryList__item" key={item.id}>
-                        <div className="inventoryList__info-column">
-                            <div className="inventoryList__column1">
-                                <label
-                                    for="item-name"
-                                    className="inventoryList__item-label">INVENTORY ITEM
-                                    <p
-                                        className="inventoryList__item-name"
-                                        name="item-name">{item.item_name}
-                                    </p>
-                                </label>
-                                <label
-                                    for="category"
-                                    className="inventoryList__category-label">CATEGORY
-                                    <p
-                                        className="inventoryList__category"
-                                        name="category">{item.category}
-                                    </p>
-                                </label>
-                            </div>
-                            <div className="inventoryList__column2">
-                                <label
-                                    for="status"
-                                    className="inventoryList__status-label">STATUS
-                                    <p
-                                        className="inventoryList__status"
-                                        name="status">{item.status}
-                                    </p>
-                                </label>
-                                <label
-                                    for="qty"
-                                    className="inventoryList__qty-label">QTY
-                                    <p
-                                        className="inventoryList__qty"
-                                        name="qty">{item.quantity}
-                                    </p>
-                                </label>
-                                <label
-                                    for="warehouse"
-                                    className="inventoryList__warehouse-label">WAREHOUSE
-                                    <p
-                                        className="inventoryList__warehouse"
-                                        name="warehouse">{item.warehouse_name}
-                                    </p>
-                                </label>
-                            </div>
-                        </div>
-                        <div className="inventoryList__interaction">
-                            <Link to="/"><img src={DeleteButton} alt="delete-button" /></Link>
-                            <Link to="/inventory/edit/"><img src={EditButton} alt="edit inventory item button" /></Link>
-                        </div>
-                    </li>
-                )}
-            </ul>
+            <table className="inventoryList">
+                <thead className="inventory__list-header">
+                    <tr className="inventory__list-row">
+                        <th className="inventory__list-heading">
+                            Inventory Item
+                            <img src={sortIcon} alt="Sort Icon" className="sort__icon" />
+                        </th>
+                        <th className="inventory__list-heading">
+                            category
+                            <img src={sortIcon} alt="Sort Icon" className="sort__icon" />
+                        </th>
+                        <th className="inventory__list-heading">
+                            Status
+                            <img src={sortIcon} alt="Sort Icon" className="sort__icon" />
+                        </th>
+                        <th className="inventory__list-heading">
+                            QTY
+                            <img src={sortIcon} alt="Sort Icon" className="sort__icon" />
+                        </th>
+                        <th className="inventory__list-heading">
+                            warehouse
+                            <img src={sortIcon} alt="Sort Icon" className="sort__icon" />
+                        </th>
+                        <th className="inventory__list-heading inventory__list-heading-actions">Actions</th>
+                    </tr>
+                </thead>
+                <tbody className="inventory__body">
+                    {inventory.map((inventory) => (
+                        <tr key={inventory.id} className="inventory__body-row">
+                            <td className="inventory__body-cell">
+                                <span className="inventory__cell-header">inventory item</span>
+                                <Link
+                                    to={`/inventory/${inventory.id}`}
+                                    className="inventory__link" >
+                                    {inventory.item_name}
+                                    <img
+                                        src={rightAarrowIcon}
+                                        alt="Right arrow Icon"
+                                        className="rightarrow__icon" />
+                                </Link>
+                            </td>
+
+                            <td className="inventory__body-cell">
+                                <span className="inventory__cell-header">Category</span>
+                                {`${inventory.category}`}
+                            </td>
+
+
+                            <td className="inventory__body-cell">
+                                <span className="inventory__cell-header">status</span>
+                                {inventory.status}
+                            </td>
+
+                            <td className="inventory__body-cell">
+                                <span className="inventory__cell-header">
+                                    qty
+                                </span>
+                                <p className="inventory__contact">
+                                    {inventory.quantity}
+                                </p>
+                                <p className="inventory__contact-info">
+                                    <a
+                                        href={`mailto:${inventory.contact_email}`}
+                                        className="inventory__contact-email">
+                                        {inventory.contact_email}
+                                    </a>
+                                </p>
+                            </td>
+
+                            <td className="inventory__body-cell inventory__actions">
+                                <button
+                                    className="inventory__delete"
+                                    onClick={() => handleDeleteClick(inventory)}>
+                                    <img
+                                        src={deleteIcon}
+                                        alt="Delete Icon"
+                                        className="delete__icon"
+                                    />
+                                </button>
+                                <Link
+                                    to={`/warehouse/${inventory.warehouse_id}`}
+                                    className="warehouse__edit">
+                                    <img src={editIcon} alt="Edit Icon" className="edit__icon" />
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
